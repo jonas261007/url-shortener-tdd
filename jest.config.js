bc833e0a16c8e0db2f4a9caf59e950aed61154a3
jonas261007~ <jonasrdl261007@gmail.com>
2025-08-14 12:48:51 -0300

    chore: configuração inicial do projeto (TypeScript, ESLint, Prettier, Jest, dotenv)

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+};
